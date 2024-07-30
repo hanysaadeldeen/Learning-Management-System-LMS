d05b479fe703f8e8b7fedd602fd3a42fbe3a2cdb
HanyMohamed <hsaad9231@gmail.com>
2024-07-30 16:11:20 +0300

    Prepare For All Course Page

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,33 @@
-"use client";
+// "use client";
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { DataTable } from "./_components/data-table";
+import { columns } from "../courses/_components/columns";
+const TeacherCourses = async () => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  const GetAllCourses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      created_at: "desc",
+    },
+  });
 
-const TeacherCourses = () => {
+  console.log(GetAllCourses);
+
   return (
     <section className="p-4">
       <Link href={"/teacher/create"}>
         <Button> Create Course</Button>
       </Link>
+      <DataTable columns={columns} data={GetAllCourses} />
     </section>
   );
 };
